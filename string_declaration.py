6c6fe8b2fcb964a82dd34b3924d2f553334c8274
MmdNazim <miamdnazim@gmail.com>
2025-11-18 12:36:03 +0600

    mutable data type and string declaration

---
+++ string_declaration.py
@@ -0,0 +1,55 @@
+#string - In Python, a string is a sequence of characters enclosed in quotes.
+#They are immutable data type, meaning they cannot be changed once created.
+
+
+"""
+#Single Quote(''):
+name1 = 'Hello Fuckers!'
+print(name1)
+
+name2 = 'He said that "You guys F!"'
+print(name2)
+"""
+
+
+"""
+#Double Quote(""):
+name1 = "Hello Fuckers!"
+print(name1)
+
+name2 = "He said that 'You guys F!'"
+print(name2)
+"""
+
+
+"""
+#Triple single quote(''' '''):
+name = '''Hello, fuckers 
+you fuckers need a 
+Daddy &
+I AM YOUR DADDY!
+'''
+print(name)
+
+name1 = '''Hello, fuckers 
+you fuckers need a 
+"Daddy" &
+I AM YOUR DADDY!
+'''
+print(name1)
+"""
+
+#Triple double quote(""" """):
+name = """ Hello, fuckers! 
+you fuckers need a 
+Daddy &
+I AM YOUR DADDY!
+"""
+print(name)
+
+name1 = """ Hello, fuckers! 
+you fuckers need a 
+'Daddy' &
+I AM YOUR DADDY!
+"""
+print(name1)
