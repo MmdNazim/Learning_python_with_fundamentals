b4e2ced34aff9515638797ffdb66078892f7d6d0
MmdNazim <miamdnazim@gmail.com>
2025-12-03 01:22:24 +0600

    console program with user input

---
+++ console_program.py
@@ -0,0 +1,40 @@
+"""
+#Greeting program:
+name = input("Enter your name:")
+print("Hello " + name + "!")
+"""
+
+"""
+#calculator program:
+num1 = input("enter  your first number:")
+num2 = input("enter your second number:")
+
+num1 = int(num1)
+num2 = int(num2)
+
+sum = num1 + num2
+
+print("The sum is:", sum)
+"""
+
+"""
+#Temperature calculator:
+celsius = input("Enter celsius:")
+celsius = float(celsius)
+fahrenheit = (celsius * 9/5) + 32
+
+print(fahrenheit)
+"""
+
+#Interest calculator:
+principal = input("Enter principal amount:")
+rate = input("Enter rate of interest:")
+time = input("Enter the time(in years):")
+
+principal = float(principal)
+rate = float(rate)
+time = float(time)
+
+interestC = (principal*rate*time)/100
+
+print(interestC)
