81a11fd54cad4f98cf03e7b66e895bf12c885854
MmdNazim <miamdnazim@gmail.com>
2025-12-05 12:03:02 +0600

    learning lambda function

---
+++ function_lambda.py
@@ -0,0 +1,25 @@
+"""
+lambda function :
+- small function ekta, anonymous function ba naam bihin function
+- eita ekta inline function ja ek line lekha lage
+- "lambda" ei keyword ta use kra hoy
+"""
+
+#simple lambda function declaration:
+"""
+result = lambda : 2+3
+print(result())
+
+#lambda function parameters:
+
+result = lambda x, y: x + y
+
+print(result(10,30))
+"""
+
+#lambda funcuions conditions:
+"""
+result = lambda x, y: x if x > y else y
+
+print(result(10,30))
+"""
