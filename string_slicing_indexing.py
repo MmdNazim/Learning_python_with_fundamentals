f2419e0a2fbd9a396553c7bb88a23aa9a42a2156
MmdNazim <miamdnazim@gmail.com>
2025-12-13 18:47:39 +0600

    learning string index slicing

---
+++ string_slicing_indexing.py
@@ -0,0 +1,34 @@
+"""
+#Positive Indexing:
+t = "Hello World"
+
+print(t[4])
+"""
+
+"""
+#Negative Indexing: "-1" theke negative indexing start hoy
+a = "Hey Nazim"
+
+print(a[-3])
+print(a[-7])
+"""
+
+"""
+#start string slicing:
+text = "Happy Hour"
+print(text[0 : 5])
+print(text[ : 9])
+"""
+
+"""
+#stop string slicing:
+s = "sahku dada"
+
+print(s[6 : ])
+"""
+
+#step string slicing:
+e = "Hi Friends"
+
+print(e[0 :: 3])
+print(e[0 :: 2])
