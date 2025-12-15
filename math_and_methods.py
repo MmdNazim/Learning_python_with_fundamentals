5c60feaea46cfcab3d2bb68cdb963568d4056c54
MmdNazim <miamdnazim@gmail.com>
2025-12-15 23:00:14 +0600

    learning math and methods in python

---
+++ math_and_methods.py
@@ -0,0 +1,43 @@
+import math
+
+#square root:
+print(math.sqrt(16))
+
+#power(4, 2):
+print(math.pow(4, 2))
+
+#Trigonometric function:
+print(math.sin(math.radians(90)))
+print(math.cos(math.radians(90)))
+
+#Logarithmic function:
+print(math.log(10))
+print(math.log10(10))
+
+#factorial:
+print(math.factorial(5))
+
+
+#absolute value:
+print(math.fabs(-7.7))
+
+#floor and ceiling:
+print(math.floor(3.3))
+print(math.ceil(3.7))
+
+
+#constants:
+print(math.pi)
+print(math.e)
+
+#Greatest Common Divisor(GCD):
+num1 = 12
+num2 = 18
+print(math.gcd(num1, num2))
+
+#Least Common Multiple(LCM):
+print(math.lcm(num1, num2))
+
+
+
+
