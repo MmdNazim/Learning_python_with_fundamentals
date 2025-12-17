f00e24f37cdd20ca90d4cab986b7a48372a6dfc4
MmdNazim <miamdnazim@gmail.com>
2025-12-17 20:26:02 +0600

    learning numbers in python

---
+++ numbers.py
@@ -0,0 +1,24 @@
+#Basic arithmetic operation:
+a = 10
+b = 3
+c = 4.33
+
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a / b)
+print("Remainder: ", a % b)
+print("Modulus: ", a % b)
+print("Exponent: ", a ** b)
+print("Floor: ", a // b)
+# print("Bits: ", a % b)
+
+#Type Conversion:
+x = 20
+y = 3.14
+
+print("Int to float:", float(x))
+print("Float to Int:", int(y))
+print("Int to Complex:", complex(x))
+
+
