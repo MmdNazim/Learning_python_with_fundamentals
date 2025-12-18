b755e2c21d206bca8dbff4449dc5fc0187688a29
MmdNazim <miamdnazim@gmail.com>
2025-12-18 20:10:57 +0600

    learning string methods in python

---
+++ string_method.py
@@ -0,0 +1,102 @@
+text = "   "
+text1 = "hello world"
+text2 = "HELLO WORLD"
+text6 = "Hello World"
+
+"""
+#convert to upercase:
+print(text1.upper())
+
+#convert to lowercase:
+print(text2.lower())
+
+#capitalize the first letter:
+print(text1.capitalize())
+
+#Title case:
+print(text1.title())
+
+#swap case:
+print(text1.swapcase())
+
+
+#Replace a substring:
+print(text1.replace("world", "python"))
+
+
+#split the string into a list:
+text3 = "hello-world-with-python"
+print(text3.split("-"))
+
+#join a list into a string:
+text4 = ['hello', 'world', 'with', 'python']
+print("-".join(text4))
+
+#strip whitespace from both ends:
+text5 = "   hello world   "
+print(text5.strip())
+
+#remove leading whitespace:
+print(text5.lstrip())
+
+#remove trailing whitespace:
+print(text5.rstrip())
+"""
+
+"""
+#check if string starts with a substring:
+print(text1.startswith("hello"))
+
+#check if string ends with a substring:
+print(text1.endswith("world"))
+
+#Find the position of substring:
+print(text1.find("world"))
+
+#count occurrences of a substring:
+print(text1.count("l"))
+
+#check if all characters are alphanumeric:
+print(text1.isalnum())
+
+#check if all characters are alphabetic:
+print(text1.isalpha())
+
+#check if all characters are digits:
+print(text1.isdigit())
+
+#check if the string contains only whitespace:
+print(text.isspace())
+
+#check if the string is titlecase:
+print(text6.istitle())
+print(text1.istitle())
+"""
+
+
+sentence = "this is a sample sentence"
+
+#capitalizing each word in sentence:
+print(sentence.title())
+
+#removing extra space and converting to uppercase:
+txt = "   hello world   "
+print(txt.strip().upper())   #[NOTE: that's the way how we'll applying the multiple method at a time]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
